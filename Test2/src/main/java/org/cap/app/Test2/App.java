59a30612e1d11b32259a5fb631b971d450daced3
lnc <lnc@DIN76000821.corp.capgemini.com>
2017-11-23 17:02:36 +0530

    commit test2

---
+++ Test2/src/main/java/org/cap/app/Test2/App.java
@@ -0,0 +1,13 @@
+package org.cap.app.Test2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
